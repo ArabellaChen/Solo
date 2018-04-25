5a79d152486ea3f0c6a4b98fba1e873913037f1a
ArabellaChen <brcongcong@gmail.com>
2018-04-24 18:32:49 -0700

    add login page

--- index.js
+++ index.js
@@ -1,40 +1,70 @@
 var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
 var path = require('path');
+var bodyParser = require('body-parser');
+
+var session = require('client-sessions');
+var cookieParser = require('cookie-parser');
 
 var api = new ParseServer({
-  databaseURI: process.env.DATABASE_URI, // Connection string for your MongoDB database
-  cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js', // Absolute path to your Cloud Code
-  appId: process.env.APP_ID,
-  masterKey: process.env.MASTER_KEY, // Keep this key secret!
-  fileKey: process.env.FILE_KEY,
-  serverURL: process.env.SERVER_URL, // Don't forget to change to https if needed
-  liveQuery: {
-    classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
-  }
+    databaseURI: process.env.DATABASE_URI, // Connection string for your MongoDB database
+    cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js', // Absolute path to your Cloud Code
+    appId: process.env.APP_ID,
+    masterKey: process.env.MASTER_KEY, // Keep this key secret!
+    fileKey: process.env.FILE_KEY,
+    serverURL: process.env.SERVER_URL, // Don't forget to change to https if needed
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
 // javascriptKey, restAPIKey, dotNetKey, clientKey
 
 var app = express();
+
+
 app.set('views', 'views');
 app.set('view engine', 'ejs');
 // Serve static assets from the /public folder
 app.use('/public', express.static(path.join(__dirname, '/public')));
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(bodyParser.json());
 
 // Serve the Parse API on the /parse URL prefix
 var mountPath = process.env.PARSE_MOUNT || '/parse';
 app.use(mountPath, api);
 
 
-// Parse Server plays nicely with the rest of your web routes
 app.get('/', function(req, res) {
-  res.status(200).send('I dream of being a website.  Please star the parse-server repo on GitHub!');
+    res.render('pages/whoAmI', {
+        active: 'whoAmI'
+    });
 });
 
-app.get('/plan', function(req, res) {
-  res.render('pages/calendar');
+app.get('/login', function(req, res) {
+    res.render('pages/login', {
+        active: 'login'
+    });
+});
+
+app.post('/login', function(req, res) {
+    console.log(req.body.username)
+    console.log(req.body.password)
+    res.send('done')
+});
+
+app.get('/signUp', function(req, res) {
+    res.render('pages/signUp', {
+        active: 'login'
+    });
+});
+
+app.get('/planer', function(req, res) {
+    res.render('pages/planer', {
+        active: 'planer'
+    });
 });
 
 
--- keys.sh
+++ keys.sh
@@ -2,12 +2,16 @@
 echo "setting keys"
 export DATABASE_URI='mongodb://localhost:27017/dev'
 
-export APP_ID="Hello World"
+# export APP_ID="Hello World"
+#
+# export MASTER_KEY="Time to do something"
 
-export MASTER_KEY="Time to do something"
+export APP_ID="Arabella"
+
+export MASTER_KEY="Solo"
 
 export FILE_KEY="optional file key"
 
 export SERVER_URL=http://localhost:1337/parse
 
-export APP_NAME= ParseServer
+export APP_NAME= ArabellaSolo
